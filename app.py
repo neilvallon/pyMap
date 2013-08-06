bd9c4aaaaabf91702bb120a3f20ba3b83cacfaea
Neil Vallon <neilvallon@gmail.com>
2013-08-06 03:40:21 -0500

    Add island remover and WIP spawn finder.

--- app.py
+++ app.py
@@ -19,6 +19,12 @@ class MapGenerator:
 		y = int(self.rand.uniform(0, self.y-1)) % self.y#int(self.rand.normalvariate(self.y/2, self.y) % self.y)
 		return (x, y)
 	
+	def randomPlayable(self):
+		point = self.randomCord()
+		while not self.isPlayArea(point):
+			point = self.randomCord();
+		return point
+	
 	def neighborCord(self, point):
 		ptList = []
 		(x, y) = point
@@ -29,8 +35,16 @@ class MapGenerator:
 		ptList.remove(point)
 		return ptList
 	
+	def manhatanDist(self, pt1, pt2):
+		return abs(pt1[0] - pt2[0]) + abs(pt1[1] - pt2[1])
+	
+	def moveableNeighbors(self, point):
+		(x, y) = point
+		ptList = [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
+		return filter(self.isPlayArea, ptList)
+	
 	def makeRandom(self):
-		for i in range(self.x * self.y):
+		for i in range(int(self.x * self.y * 0.75)):
 			(x, y) = self.randomCord()
 			self.mapMatrix[y][x] ^= 1
 		return self
@@ -49,20 +63,75 @@ class MapGenerator:
 		self.mapMatrix = newMap
 		return self
 	
+	def removeIslands(self):
+		newMap = self.emptyMap(self.x, self.y)
+		
+		point = self.randomPlayable()
+		
+		frontier = [point]
+		explored = []
+		while frontier:
+			node = frontier.pop()
+			neighbors = self.moveableNeighbors(node)
+			toAdd = filter(lambda x: not (x in frontier or x in explored), neighbors)
+			
+			frontier.extend(toAdd)
+			explored.append(node)
+			newMap[node[1]][node[0]] = 1
+		
+		self.mapMatrix = newMap
+		return self
+	
+	def findSpawns(self):
+		posibleSpawns = self.playableCords()
+		s1 = self.randomCord()
+		s2 = self.randomCord()
+		
+		for i in range(3): ## Do 3 iterations
+			s1acum = (0, 0)
+			s2acum = (0, 0)
+			s1c = 0
+			s2c = 0
+			
+			for s in posibleSpawns:
+				s1Dis = self.manhatanDist(s, s1)
+				s2Dis = self.manhatanDist(s, s2)
+				if s1Dis < s2Dis:
+					s1c += 1
+					s1acum = (s1acum[0] + s[0], s1acum[1] + s[1])
+				else:
+					s2c += 1
+					s2acum = (s2acum[0] + s[0], s2acum[1] + s[1])
+			
+			s1 =  (s1acum[0] / s1c, s1acum[1] / s1c)
+			s2 =  (s2acum[0] / s2c, s2acum[1] / s2c)
+		
+		self.spawns = ((int(s1[0]), int(s1[1])), (int(s2[0]), int(s2[1])))
+		return self
+	
 	def isPlayArea(self, point):
 		(x, y) = point
+		if(x<0 or y<0 or x >= self.x or y >= self.y):
+			return False
 		return self.mapMatrix[y][x] == 1
 	
 	def isWall(self, point):
-		if not self.isPlayArea(point):
-			return False
-		
 		neighbors = self.neighborCord(point)
-		allNeighborsPlayable = len(neighbors) == 8
-		for n in neighbors:
-			allNeighborsPlayable &= self.isPlayArea(n)
-		return not allNeighborsPlayable
+		if self.isPlayArea(point):
+			return len(neighbors) != 8
 		
+		allNeighborsPlayable = False
+		for n in neighbors:
+			allNeighborsPlayable |= self.isPlayArea(n)
+		return allNeighborsPlayable
+	
+	def playableCords(self):
+		ptLst = []
+		for y in range(self.y):
+			for x in range(self.x):
+				if self.isPlayArea((x, y)):
+					ptLst.append((x, y))
+		return ptLst
 	
 	def matrix(self):
 		return self.mapMatrix
@@ -86,7 +155,9 @@ class MapGenerator:
 		colorMap = self.emptyMap(self.x, self.y)
 		for x in range(self.x):
 			for y in range(self.y):
-				if self.isWall((x, y)):
+				if (x, y) in self.spawns:
+					colorMap[y][x] = 'dropper_front_horizontal.png'
+				elif self.isWall((x, y)):
 					colorMap[y][x] = 'stonebrick_mossy.png'
 				elif self.isPlayArea((x, y)):
 					colorMap[y][x] = 'sand.png'
@@ -124,7 +195,58 @@ def index(seed=300):
 def index(width=50, height=50, seed=300):
 	tstart = datetime.now()
 	m = MapGenerator(int(width), int(height), seed)
-	m.makeRandom().smooth().smooth().smooth()
+	m.makeRandom().smooth().smooth().smooth().removeIslands().findSpawns()
+	
+	tdelta = datetime.now()-tstart
+	
+	html = """
+		<html>
+		<head>
+		<title>pymap</title>
+		<style>
+		table, tr, td, img{
+		border:none;
+		padding:0px;
+		margin:-1px;
+		}
+		body{
+			background-image:URL("/img/wool_colored_black.png");
+		}
+		h2{
+		color:white;
+		}
+		</style>
+		</head>
+		<body>"""
+	
+	html += "<h2>Generated in: "+str(tdelta)+"</h2>"
+	
+	html += """
+		<center>
+		<table>"""
+	
+	for y in m.imageform():
+		html += "<tr>"
+		for x in y:
+			html += "<td><img src='/img/"+x+"' /></td>"
+		html += "</tr>"
+	
+	
+	html += """
+	</table>
+	</center>
+	</body>
+	</html>
+	"""
+	
+	return html
+
+
+@route('/')
+def index(width=50, height=50):
+	tstart = datetime.now()
+	m = MapGenerator(int(width), int(height), random.random())
+	m.makeRandom().smooth().smooth().smooth().removeIslands().findSpawns()
 	
 	tdelta = datetime.now()-tstart
 	
