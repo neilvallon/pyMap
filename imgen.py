f85b53204ae2f1eef260156742d99c479ace8210
Neil Vallon <neilvallon@gmail.com>
2014-05-15 10:34:24 -0500

    Reorgonize

--- imgen.py
+++ imgen.py
@@ -3,26 +3,31 @@ from MapGenerator import *
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-def index(seed=300):
-	m = MapGenerator(100, 100, seed)
-	m.makeRandom()
-	
+def smoothPlot(m):
 	fig = plt.figure()
 	fig.suptitle('No smoothing', fontsize=20, fontweight='bold')
-	
+
+	## Flip matrix values
 	out = np.logical_xor(m.mapMatrix, True)
 	plt.imshow(out, cmap='binary', interpolation='nearest')
+
 	plt.savefig('img/initial.png')
-	
+
 	for n in xrange(3):
+		m.smooth()
+
 		fig = plt.figure()
 		fig.suptitle('Smooth Factor - %s' % n, fontsize=20, fontweight='bold')
-		m.smooth()
+
 		out = np.logical_xor(m.mapMatrix, True)
 		plt.imshow(out, cmap='binary', interpolation='nearest')
+
 		plt.savefig('img/smothed_%s.png' % n)
 
+	return m
 
+seed = 300
+m = MapGenerator(100, 100, seed)
+m.makeRandom()
 
-index()
+m = smoothPlot(m)
