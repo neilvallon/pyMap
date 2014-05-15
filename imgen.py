19ff5cb9057c90996ca827ec60fa7ce27596cc78
Neil Vallon <neilvallon@gmail.com>
2014-05-15 10:27:56 -0500

    Set plot titles

--- imgen.py
+++ imgen.py
@@ -8,12 +8,16 @@ def index(seed=300):
 	m = MapGenerator(100, 100, seed)
 	m.makeRandom()
 	
+	fig = plt.figure()
+	fig.suptitle('No smoothing', fontsize=20, fontweight='bold')
 	
 	out = np.logical_xor(m.mapMatrix, True)
 	plt.imshow(out, cmap='binary', interpolation='nearest')
 	plt.savefig('img/initial.png')
 	
 	for n in xrange(3):
+		fig = plt.figure()
+		fig.suptitle('Smooth Factor - %s' % n, fontsize=20, fontweight='bold')
 		m.smooth()
 		out = np.logical_xor(m.mapMatrix, True)
 		plt.imshow(out, cmap='binary', interpolation='nearest')
